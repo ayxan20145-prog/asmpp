c1c5c83b8bcffe713d54bf4072eaee9e4a17e375
ayxan <ayxan.20145@gmail.com>
2026-09-11 10:40:26 +0400

    added next_token() method

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
+#[derive(Debug)]
 enum Token {
     Register(Register),
+    Mov,
+
+    Equals,
+
+    Semicolon,
+    Eof,
 }
 
+#[derive(Debug)]
 enum Register {
     RAX,
     RBX,
@@ -31,11 +39,73 @@ impl Lexer {
     fn advance(&mut self) {
         self.position += 1;
     }
+    fn next_token(&mut self) -> Token {
+        loop {
+            match self.current() {
+                Some(c) => {
+                    if c.is_whitespace() {
+                        self.advance();
+                    } else {
+                        break;
+                    }
+                }
+                None => break,
+            }
+        }
+
+        match self.current() {
+            None => Token::Eof,
+
+            Some(';') => {
+                self.advance();
+                Token::Semicolon
+            }
+
+            Some('=') => {
+                self.advance();
+                Token::Equals
+            }
+
+            Some(c) if c.is_alphabetic() => {
+                let mut name = String::new();
+
+                loop {
+                    match self.current() {
+                        Some(c) => {
+                            if c.is_alphanumeric() {
+                                name.push(c);
+                                self.advance();
+                            } else {
+                                break;
+                            }
+                        }
+                        None => break,
+                    }
+                }
+
+                if name == "mov" {
+                    Token::Mov
+                } else {
+                    Token::Register(match name.as_str() {
+                        "rax" => Register::RAX,
+                        "rbx" => Register::RBX,
+                        "rcx" => Register::RCX,
+                        "rdx" => Register::RDX,
+                        "rsi" => Register::RSI,
+                        "rdi" => Register::RDI,
+                        "rbp" => Register::RBP,
+                        "rsp" => Register::RSP,
+                        _ => panic!("error message"),
+                    })
+                }
+            }
+            Some(c) => panic!("unexpected char: {}", c),
+        }
+    }
 }
 
 fn main() {
-    let mut lexer = Lexer::new("mov rax, 1");
-    println!("{}", lexer.current().unwrap());
-    lexer.advance();
-    println!("{}", lexer.current().unwrap());
+    let mut lexer = Lexer::new("rax = rbx");
+    let token = lexer.next_token();
+    println!("{:?}", token);
 }
